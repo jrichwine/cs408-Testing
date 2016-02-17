9cc38d2ec4013dbfc95dc07829c77e0d29b7fcad
Jeremiah Murphy <jeremiah.g.murphy@gmail.com>
2016-02-17 18:09:50 -0500

    listview pulls from building list

--- BoilerCheck/app/src/main/java/com/cs408/team13/BoilerCheck/ListActivity.java
+++ BoilerCheck/app/src/main/java/com/cs408/team13/BoilerCheck/ListActivity.java
@@ -2,12 +2,19 @@ package com.cs408.team13.BoilerCheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        list = (ListView) findViewById(R.id.building_list);
+        list.setAdapter(new BuildingListAdapter(this));
     }
 }
