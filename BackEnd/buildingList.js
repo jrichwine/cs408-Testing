51fb953f98177b7c6e5968bb6147c5ef28073946
Nadeem Mahmood <mahmoodn@purdue.edu>
2016-02-19 13:47:46 -0500

    Location class

---
+++ BackEnd/buildingList.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+{buildings": [
+	{"buildingName":"Hicks Undergraduate Library", "longitude":"", "latitude":"", "type":"study"},
+	{"buildingName":"Earhart Dining Court", "longitude":"", "latitude":"", "type":"eat"},
+	{"buildingName":"France A. Córdova Recreational Sports Center", "longitude":"-86.921709", "latitude":"40.424254", "type":"fun"}
+]}
